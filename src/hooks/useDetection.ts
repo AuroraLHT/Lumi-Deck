34229339605e32de4de0642d6ee4ccf229faaa2f
AuroraLHT <auroralht@gmail.com>
2024-09-25 19:40:23 -0400

    add filter to reduce state update time

--- src/hooks/useDetection.ts
+++ src/hooks/useDetection.ts
@@ -5,11 +5,11 @@ import useLiveAnalysisClient from "../clients/liveAnalysis/analyzer";
 
 const useDetection = () => {
   // const detectorNodeState  = useDetectorNodeStore( s => s.state );
-  const store = useLiveAnalysisClient();
-  const bboxes = store.bboxes;
-  const cropSetup = store.cropSetup;
-  const socket = store.socket;
-  const isConnected = store.isConnected;
+  // const store = useLiveAnalysisClient();
+  const bboxes = useLiveAnalysisClient(s=>s.bboxes);
+  const cropSetup = useLiveAnalysisClient(s=>s.cropSetup);
+  const socket = useLiveAnalysisClient(s=>s.socket);
+  const isConnected = useLiveAnalysisClient(s=>s.isConnected);
   // console.log(store.cache);
   return { bboxes, cropSetup, socket, isConnected };
 };
